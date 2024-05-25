dcea4d842f92b1f4c3d952d462440424fbcae7c4
ngaurav <nishantgaurav@icloud.com>
2024-05-25 11:15:02 +0530

    ability to draw grids

--- step4.py
+++ step4.py
@@ -10,10 +10,28 @@ from os.path import isfile, join
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+# Font Variables
 prominence_ranges = [0.75,0.5,0.3,0.2,0.15,0.1]
 font_sizes = [1,0.6,0.35,0.2,0.15]
-spacing = [40,20,10,6,4]
+line_spacing = [40,20,10,6,4]
 font_factor = 48.0
+
+class Grid(object):
+    """ Helper class to encapsulate the grid properties of graphic
+    """
+
+    def __init__(self, cols, rows, width, height) -> None:
+        self.width = width
+        self.height = height
+        self.cols = cols
+        self.rows = rows
+        self.gutter_margin = round(height/40)
+        vertical_padding = self.gutter_margin * 2
+        horizontal_padding = round(width/cols/4)
+        self.col_spacing = round((width-2*horizontal_padding)/cols)
+        self.row_spacing = round((height-2*vertical_padding)/rows)
+        self.vertical_padding = (height-self.row_spacing*rows)/2
+        self.horizontal_padding = (width-self.col_spacing*cols)/2
 
 from PIL import Image, ImageDraw
 
@@ -102,39 +120,38 @@ def update_font_factor(font, text, priority, canvas_width, canvas_height):
             print ("condition 3 ", font_factor)
             return font_size
         
-def draw(width, height, elements, font):
-    rows = 3
-    cols = 3
-    half_gutter_spacing = 10
+def draw(width, height, font, grid:Grid):
+    rows = grid.rows
+    cols = grid.cols
+    half_gutter_spacing = grid.gutter_margin/2
     image = Image.new("RGB", (width, height), "white")
     draw = ImageDraw.Draw(image)
-    row_spacing = height // rows
-    col_spacing = width // cols
-    for i in range(1, rows):
-        draw.line((0, i * row_spacing - half_gutter_spacing, width, i * row_spacing - half_gutter_spacing), fill="black", width=2)
-        draw.line((0, i * row_spacing + half_gutter_spacing, width, i * row_spacing + half_gutter_spacing), fill="black", width=2)
+    row_spacing = grid.row_spacing
+    col_spacing = grid.col_spacing
+    for i in range(0, rows+1):
+        draw.line((0, grid.vertical_padding + i * row_spacing - half_gutter_spacing, width, grid.vertical_padding + i * row_spacing - half_gutter_spacing), fill="black", width=2)
+        draw.line((0, grid.vertical_padding + i * row_spacing + half_gutter_spacing, width, grid.vertical_padding + i * row_spacing + half_gutter_spacing), fill="black", width=2)
 
-    for i in range(1, cols):
-        draw.line((i * col_spacing - half_gutter_spacing, 0, i * col_spacing - half_gutter_spacing, height), fill="black", width=2)
-        draw.line((i * col_spacing + half_gutter_spacing, 0, i * col_spacing + half_gutter_spacing, height), fill="black", width=2)
+    for i in range(0, cols+1):
+        draw.line((grid.horizontal_padding + i * col_spacing - half_gutter_spacing, 0, grid.horizontal_padding + i * col_spacing - half_gutter_spacing, height), fill="black", width=2)
+        draw.line((grid.horizontal_padding + i * col_spacing + half_gutter_spacing, 0, grid.horizontal_padding + i * col_spacing + half_gutter_spacing, height), fill="black", width=2)
 
-    cols = [0,0,0,1,1,1,2,2,2]
-    rows = [0,1,2,0,1,2,0,1,2]
-    for i in range(len(elements)):
-        col = cols[i]
-        row = rows[i]
-        text = elements[i]['description']
-        print(text)
-        text_position = (col * col_spacing + half_gutter_spacing, row * row_spacing + half_gutter_spacing)
-        priority = elements[i]['prominence']
-        ft = font.font_variant(size=font_sizes[priority-1] * font_factor)
-        # to test multiline support remove the comment in the following line 
-        max_width = prominence_ranges[priority-1] * width # * 0.8
-        wrapper = TextWrapper(text, ft, max_width)
-        wrapped_text = wrapper.wrapped_text()
-        draw.multiline_text(text_position, wrapped_text, spacing=spacing[priority-1], fill="black", font=ft)    
+    # cols = [0,0,0,1,1,1,2,2,2]
+    # rows = [0,1,2,0,1,2,0,1,2]
+    # for i in range(len(elements)):
+    #     col = cols[i]
+    #     row = rows[i]
+    #     text = elements[i]['description']
+    #     print(text)
+    #     text_position = (col * col_spacing + half_gutter_spacing, row * row_spacing + half_gutter_spacing)
+    #     priority = elements[i]['prominence']
+    #     ft = font.font_variant(size=font_sizes[priority-1] * font_factor)
+    #     # to test multiline support remove the comment in the following line 
+    #     max_width = prominence_ranges[priority-1] * width # * 0.8
+    #     wrapper = TextWrapper(text, ft, max_width)
+    #     wrapped_text = wrapper.wrapped_text()
+    #     draw.multiline_text(text_position, wrapped_text, spacing=line_spacing[priority-1], fill="black", font=ft)    
     return image
-
 
 if __name__ == "__main__":
     output_folder = os.path.join(config['STEP4_OUTPUT_FOLDER'])
@@ -148,8 +165,23 @@ if __name__ == "__main__":
     for file in onlyfiles:
         with open(join(config['STEP4_INPUT_FOLDER'],file), 'r') as f:
             step4_input = json.load(f)
-        for el in step4_input['elements']:
-            update_font_factor(font=font, text=el['description'], priority=el['prominence'], canvas_height=step4_input['height'], canvas_width=step4_input['width'])
-        img = draw(height=step4_input['height'], width=step4_input['width'], elements=step4_input['elements'],font=font)
+        grid = Grid(
+            height=step4_input['height'],
+            width=step4_input['width'],
+            cols=step4_input['grid']['cols'],
+            rows=step4_input['grid']['rows']
+        )
+        # for el in step4_input['elements']:
+        #     update_font_factor(
+        #         font=font,
+        #         text=el['description'],
+        #         priority=el['prominence'],
+        #         canvas_height=step4_input['height'],
+        #         canvas_width=step4_input['width'])
+        img = draw(
+            height=step4_input['height'],
+            width=step4_input['width'],
+            font=font,
+            grid=grid)
         img.save(join(config['STEP4_OUTPUT_FOLDER'],f"{file}.png"))
         i = i + 1
